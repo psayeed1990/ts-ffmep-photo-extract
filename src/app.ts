7a272b87fc070c145fe846846bc9f25df455e72f
Md. Abu Sayeed Mondal <sayeedmondal1412@gmail.com>
2021-12-22 11:19:05 +0600

    added basic pages

---
+++ src/app.ts
@@ -0,0 +1,56 @@
+import express, { Application } from 'express';
+import compression from 'compression';
+import cors from 'cors';
+import morgan from 'morgan';
+import expressRateLimit from 'express-rate-limit';
+import Controller from '@/utils/interfaces/controller.interface';
+import ErrorMiddleware from '@/middlewares/error.middleware';
+import helmet from 'helmet';
+
+class App {
+    public express: Application;
+    public port: number;
+
+    constructor(controllers: Controller[], port: number) {
+        this.express = express();
+        this.port = port;
+
+        this.initialiseDatabaseConnection();
+        this.initialiseMiddleware();
+        this.initialiseControllers(controllers);
+        this.initialiseErrorHandling();
+    }
+
+    private initialiseMiddleware(): void {
+        this.express.use(helmet());
+        this.express.use(cors());
+        this.express.use(
+            expressRateLimit({
+                windowMs: 15 * 60 * 1000, // 15 minutes
+                max: 100, // limit each IP to 100 requests per windowMs
+            })
+        );
+        this.express.use(morgan('dev'));
+        this.express.use(express.json({ limit: '20kb' }));
+        this.express.use(express.urlencoded({ extended: false }));
+        this.express.use(compression());
+    }
+
+    private initialiseControllers(controllers: Controller[]): void {
+        controllers.forEach((controller: Controller) => {
+            this.express.use('/api', controller.router);
+        });
+    }
+
+    private initialiseErrorHandling(): void {
+        this.express.use(ErrorMiddleware);
+    }
+
+    public listen(): void {
+        this.express.listen(this.port, () => {
+            console.log(`App listening on the port ${this.port}`);
+        });
+    }
+}
+
+export default App;
