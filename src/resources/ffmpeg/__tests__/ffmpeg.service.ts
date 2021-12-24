70f64ed308a213edfcc39cef8b6a0bed1b477152
Md. Abu Sayeed Mondal <sayeedmondal1412@gmail.com>
2021-12-24 15:00:19 +0600

    check if valid png

--- src/resources/ffmpeg/__tests__/ffmpeg.service.ts
+++ src/resources/ffmpeg/__tests__/ffmpeg.service.ts
@@ -4,7 +4,7 @@
 // import { expect } from 'chai';
 import FFMPEGService from '@/resources/ffmpeg/ffmpeg.service';
 import Photo from '@/resources/ffmpeg/ffmpeg.interface';
-import { Assertion } from 'chai';
+import Jimp from 'jimp';
 
 //define photo
 let photo: Photo;
@@ -33,6 +33,28 @@ describe('FFMPEGService', () => {
     it('Extracted photo should not be a empty string ', async () => {
         expect(photo.data).not.toEqual('');
         expect(photo.data).not.toEqual(' ');
+    });
+
+    //test photo data to be base64
+    it('Extracted photo should be a base64 string', async () => {
+        expect(photo.data).toMatch(
+            /^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$/
+        );
+    });
+
+    //test photo data string to be png type
+    it('Extracted photo should be a valid PNG image', async () => {
+        const buffer = Buffer.from(photo.data, 'base64');
+
+        const image = await Jimp.read(buffer);
+        expect(image.getExtension()).toEqual('png');
+
+        //test image with puppeteer
+        // const browser = await puppeteer.launch();
+        // const page = await browser.newPage();
+        // await page.goto(`data:image/png;base64, ` + photo.data);
+        // await page.screenshot({ path: './test.png' });
+        // await browser.close();
     });
 });
 
