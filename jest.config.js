b6cf7e9e1807aede62ab14fd2cff4e30c3b292d7
Md. Abu Sayeed Mondal <sayeedmondal1412@gmail.com>
2021-12-24 13:06:51 +0600

    added module mapper and jest config

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,38 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+
 module.exports = {
     preset: 'ts-jest',
-    testEnvironment: 'node',
-    moduleNameMapper: {
-        '@exmpl/(.*)': '<rootDir>/src/$1',
+
+    // Automatically clear mock calls and instances between every test
+    clearMocks: true,
+
+    coverageDirectory: 'coverage',
+
+    // An array of regexp pattern strings used to skip coverage collection
+    coveragePathIgnorePatterns: ['<rootDir>/node_modules/(?!@foo)'],
+
+    globals: {
+        'ts-jest': {
+            tsConfigFile: 'tsconfig.json',
+            enableTsDiagnostics: true,
+        },
     },
+
+    moduleFileExtensions: ['ts', 'tsx', 'js'],
+
+    // A map from regular expressions to module names that allow to stub out resources with a single module
+    moduleNameMapper: {
+        '@/(.*)': '<rootDir>/src/$1',
+        //database: '<rootDir>/src/lib/database',
+    },
+
+    testEnvironment: 'node',
+
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+
+    transform: {
+        '^.+\\.(ts|tsx)$': 'ts-jest',
+    },
+
+    transformIgnorePatterns: ['<rootDir>/node_modules/(?!@foo)'],
 };
